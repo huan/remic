a3fa3d5a5932f718895749cf438a43dbb53d0fc1
Huan Li <huan@chatie.io>
2025-08-23 16:54:53 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // appDir is now stable in Next.js 13+, no need for experimental flag
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/0823-04pm-landing.html',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
